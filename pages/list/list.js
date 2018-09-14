95448c653ca558e42e3c61d5ac8e2fc87cad57e4
Daniel Shell <dshell321@gmail.com>
2018-09-14 21:58:56 +1000

    Refactored header and sidemenu to be global components

--- pages/list/list.js
+++ pages/list/list.js
@@ -4,12 +4,11 @@
 $(document).on("ready", function() {
     $("#js-menu-open").on("click", function() {
         $("#js-menu").addClass("active");
-        $("#js-list-body").addClass("inactive");
+        $(".global-container").addClass("inactive");
     });
 
     $("#js-menu-close").on("click", function() {
-        console.log("CLICK");
         $("#js-menu").removeClass("active");
-        $("#js-list-body").removeClass("inactive");
+        $(".global-container").removeClass("inactive");
     });
 });
